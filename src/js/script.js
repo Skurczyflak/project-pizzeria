310bcaef5d4fa2522aa9891f337b9eadc39e1455
Mateusz Matysek <mateusziom1@gmail.com>
2025-04-26 11:36:52 +0200

    Add dropdown to cart

--- src/js/script.js
+++ src/js/script.js
@@ -271,6 +271,33 @@ class AmountWidget{
 
 }
 
+class Cart{
+  constructor(element){
+    const thisCart = this;
+
+    thisCart.products = [];
+    thisCart.getElements(element);
+    thisCart.initActions();
+    console.log('new Cart:', thisCart);
+  }
+
+  getElements(element){
+    const thisCart = this;
+
+    thisCart.dom = {};
+    thisCart.dom.wrapper = element;
+    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    
+  }
+
+  initActions(){
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+      event.preventDefault();
+      thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+    })
+  }
+}
 
   const app = {
     initData: function(){
@@ -286,6 +313,12 @@ class AmountWidget{
       }
 
     },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
     init: function(){
       const thisApp = this;
       //console.log('*** App starting ***');
@@ -296,6 +329,7 @@ class AmountWidget{
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
