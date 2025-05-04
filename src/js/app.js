70e732cbd0b2b1b0ecbd9e890519075749a8b4d3
Mateusz Matysek <mateusziom1@gmail.com>
2025-05-04 14:54:55 +0200

    Add Booking page

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,52 @@
- import {settings, select} from './settings.js';
- import Product from './components/product.js';
- import Cart from './components/cart.js';
-//  import Product from './components/Product.js'; Big letters in import cause error in IDE
-//  import Cart from './components/Cart.js'; Big letters in import cause error in IDE
+ import {settings, select, classNames} from './settings.js';
+ import Product from './components/Product.js';
+ import Cart from './components/Cart.js';
+ import Booking from './components/Booking.js';
  
  const app = {
+    initPages: function(){
+      const thisApp = this;
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      let pageMatchingHash = thisApp.pages[0].id;
+
+      for(let page of thisApp.pages){
+        if(page.id == idFromHash){
+          pageMatchingHash = page.id;
+          break;
+        }
+      }
+
+      thisApp.activatePage(pageMatchingHash);
+
+      for(let link of thisApp.navLinks){
+        link.addEventListener('click', function(event){
+          const clickedElement = this;
+          event.preventDefault();
+          const id = clickedElement.getAttribute('href').replace('#', '');
+          thisApp.activatePage(id);
+          window.location.hash = '#/' + id;
+        });
+      }
+    },
+    activatePage: function(pageId){
+      const thisApp = this;
+
+      for(let page of thisApp.pages){
+        page.classList.toggle(classNames.pages.active, page.id == pageId);
+      }
+
+      for(let link of thisApp.navLinks){
+        link.classList.toggle(
+          classNames.nav.active, 
+          link.getAttribute('href') == '#' + pageId
+        );
+      }
+
+    },
     initData: function(){
       const thisApp = this;
       thisApp.data = {};
@@ -38,10 +80,18 @@
         app.cart.add(event.detail.product);
       });
     },
+
+    initBooking: function(){
+      const thisApp = this;
+      const bookingContainer = document.querySelector(select.containerOf.booking);
+      thisApp.booking = new Booking(bookingContainer);
+    },
     init: function(){
       const thisApp = this;
       thisApp.initData();
       thisApp.initCart();
+      thisApp.initPages();
+      thisApp.initBooking();
     },
   };
 
---
+++ src/js/components/Booking.js
@@ -0,0 +1,28 @@
+import {select,templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking{
+    constructor(element){
+        const thisBooking = this;
+        thisBooking.render(element);
+        thisBooking.initWidgets();
+    }
+    render(element){
+        const thisBooking = this;
+        /* generate HTML based on template */
+        const generatedHTML = templates.bookingWidget();
+        thisBooking.dom = {};
+        /* create element using utils.createElementFromHTML */
+        thisBooking.dom.wrapper = element;
+        thisBooking.dom.wrapper.innerHTML = generatedHTML;
+        thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+        thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+
+    }
+    initWidgets(){
+        const thisBooking = this;
+        thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
+        thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+    }
+}
+export default Booking;
